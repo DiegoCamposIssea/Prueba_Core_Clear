bd93af010ebe67ed84afbd7aa07938a755a2fbf8
Saul Mascorro Luevano <diego.campos@issea.gob.mx>
2025-10-21 14:23:26 -0600

    Subida de proyecto

---
+++ Web.API/WeatherForeCast.cs
@@ -0,0 +1,9 @@
+namespace Web.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+    }
+}
