30ee90414d149fbb5041564eb34e170373d91c9b
DiegoCamposIssea <diego.campos@issea.gob.mx>
2026-02-06 14:58:31 -0600

    Feature: Implementacion de migraciones, usando como base el SQLSERVER, ademas de que se mantuvieron las versiones a 8.0.3

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8d2210df5197aba09ffdbbb739bbd39090f39b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862fb877e3c539b1f44405719252e94d1669881f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
