722d06a623f0b3060d35c118abeeafb29676f400
DiegoCamposIssea <diego.campos@issea.gob.mx>
2026-02-09 12:45:42 -0600

    Refactor: actualizacion del paquete TOOLS

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862fb877e3c539b1f44405719252e94d1669881f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ee90414d149fbb5041564eb34e170373d91c9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
